c07d67c385fca903a9684cf3bc54c9ca01dc73e5
Philipp <joh.phillipp@gmail.com>
2014-06-04 10:11:01 +0200

    PWM Treiber hinzugefügt

---
+++ LPC_pwm.c
@@ -0,0 +1,227 @@
+/**
+* @file		LPC_pwm.c 
+* @brief    Treiber für das Steuern von ein bzw. mehrerer PWM Signale \n
+* @version	V1.0 
+* @date			2014-05-31
+* @author 	112222\INS \n
+*		   			Westsächsische Hochschule Zwickau \n
+*		   			Fakultät Elektrotechnik - Informationstechnik \n
+*/ 
+#include <stdint.h>
+#include <stdlib.h>
+#include "lpc17xx.h"
+#include "pwm.h"
+
+uint32_t match_counter0, match_counter1;
+
+//Konrtollstruktur für eine PWM
+struct pwmCTRL
+{
+	uint8_t use;
+	uint8_t handle;
+};
+
+/*
+void PWM1_IRQHandler (void) 
+{
+  uint32_t regVal;
+
+  regVal = LPC_PWM1->IR;  						// Read Interrupt Flag
+  if ( regVal & MR0_INT )
+  {
+	match_counter1++;		  					// Match-0 Interrupt
+  }
+  LPC_PWM1->IR |= regVal;						// Clear Interrupt Flag on Match-0 
+  return;
+}
+*/
+
+struct pwmCTRL pwm[PWM_COUNT];
+
+/******************PWM_open*****************************************/
+/**
+ * @brief	Exlusive Reservierung 
+ * @return	void
+ * @param   Nummer der PWM
+ *******************************************************************/
+void PWM_open(uint8_t pwmNumber)
+{
+	if(pwmNumber < PWM_COUNT)
+	{
+		switch(pwmNumber)
+		{
+			case PWM1: 	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<0); 	// Reset P2.0 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<0);	// Config P2.0 = PWM1
+									break;
+			case PWM2: 	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<2); 	// Reset P2.1 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<2);	// Config P2.1 = PWM2
+									break;
+			case PWM3:	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<4); 	// Reset P2.2 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<4);	// Config P2.2 = PWM3
+									break;
+			case PWM4:	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<6); 	// Reset P2.3 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<6);	// Config P2.3 = PWM4
+									break;
+			case PWM5:	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<8); 	// Reset P2.4 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<8);	// Config P2.4 = PWM5
+									break;
+			case PWM6:	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<10);	// Reset P2.5 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<10);	// Config P2.5 = PWM6
+									break;
+		}
+	}
+}
+
+
+
+void PWM_init(/*uint8_t pwm_handle,*/ uint32_t cycle)
+{
+
+	//uintptr_t pwmConp = (uintptr_t *) PCONP_REGISTER;
+	//uintptr_t pwmPerCLK = (uintptr_t *) PCLKSELR0;
+	/*if(pwm_handle < PWM_COUNT)
+	{
+		// *pwmConp |= (PWM1_CLK_ENABLE);			//pheriphal Clock enabled
+		// *pwmPerCLK |= (PWM1_CLK_CCLK);			//set Perripheral Clock
+ 	
+		match_counter1 = 0;
+		
+		switch(pwm_handle)
+		{
+			case PWM1: 	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<0); 	// Reset P2.0 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<0);	// Config P2.0 = PWM1
+									break;
+			case PWM2: 	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<2); 	// Reset P2.1 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<2);	// Config P2.1 = PWM2
+									break;
+			case PWM3:	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<4); 	// Reset P2.2 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<4);	// Config P2.2 = PWM3
+									break;
+			case PWM4:	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<6); 	// Reset P2.3 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<6);	// Config P2.3 = PWM4
+									break;
+			case PWM5:	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<8); 	// Reset P2.4 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<8);	// Config P2.4 = PWM5
+									break;
+			case PWM6:	
+									LPC_PINCON->PINSEL4 &= ~(0x03<<10);	// Reset P2.5 = GPIO   
+									LPC_PINCON->PINSEL4 |=  (0x01<<10);	// Config P2.5 = PWM6
+									break;
+		}
+		*/
+		LPC_PWM1->TCR = TCR_RESET;			// Counter Reset 
+		//LPC_PWM1->PR = 0x00;					// Prescale = 1 -> count frequency:Fpclk(72MHz/4=18MHz)
+		LPC_PWM1->PR = 17;							// Prescale = 18 = 18MHz/18	= 1MHz
+		LPC_PWM1->MR0 = cycle;					//Main cyclce
+	//}
+
+}
+
+void PWM_set(uint8_t pwm_handle, uint32_t cycle)
+{
+
+	if(pwm_handle < PWM_COUNT)
+	{
+		switch(pwm_handle)
+		{
+			case PWM1: 	LPC_PWM1->MR1 = cycle;
+									LPC_PWM1->LER |= LER1_EN;
+									break;
+			case PWM2: 	LPC_PWM1->MR2 = cycle;	
+									LPC_PWM1->LER |= LER2_EN;
+									break;
+			case PWM3:	LPC_PWM1->MR3 = cycle;
+									LPC_PWM1->LER |= LER3_EN;
+									break;
+			case PWM4:	LPC_PWM1->MR4 = cycle;
+									LPC_PWM1->LER |= LER4_EN;
+									break;
+			case PWM5:	LPC_PWM1->MR5 = cycle;
+									LPC_PWM1->LER |= LER5_EN;
+									break;
+			case PWM6:	LPC_PWM1->MR6 = cycle;
+									LPC_PWM1->LER |= LER6_EN;
+									break;
+		}
+	}
+
+}
+
+void PWM_start(uint8_t pwm_handle)
+{
+
+	if(pwm_handle < PWM_COUNT)
+	{
+		switch(pwm_handle) //all PWM single edge
+		{
+			case PWM1: 	LPC_PWM1->PCR |= PWMENA1;
+						break;
+			case PWM2: 	LPC_PWM1->PCR |= PWMENA2;
+						break;
+			case PWM3: 	LPC_PWM1->PCR |= PWMENA3;
+						break;
+			case PWM4: 	LPC_PWM1->PCR |= PWMENA4;
+						break;
+			case PWM5: 	LPC_PWM1->PCR |= PWMENA5;
+						break;
+			case PWM6: 	LPC_PWM1->PCR |= PWMENA6;
+						break;
+		}
+
+		LPC_PWM1->PCR |= PWMENA6 | PWMENA5 | PWMENA4 | PWMENA3 | PWMENA2 | PWMENA1;
+		LPC_PWM1->TCR = TCR_CNT_EN | TCR_PWM_EN;
+		
+	}
+}
+
+
+void PWM_stop(uint8_t pwm_handle)
+{
+	if(pwm_handle < PWM_COUNT)
+	{
+		switch(pwm_handle) //all PWM single edge
+		{
+			case PWM1: 	LPC_PWM1->PCR |= PWMDISA1;
+						break;
+			case PWM2: 	LPC_PWM1->PCR |= PWMDISA2;
+						break;
+			case PWM3: 	LPC_PWM1->PCR |= PWMDISA3;
+						break;
+			case PWM4: 	LPC_PWM1->PCR |= PWMDISA4;
+						break;
+			case PWM5: 	LPC_PWM1->PCR |= PWMDISA5;
+						break;
+			case PWM6: 	LPC_PWM1->PCR |= PWMDISA6;
+						break;
+		}
+		LPC_PWM1->TCR = TCR_RESET;	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ LPC_pwm.h
@@ -0,0 +1,99 @@
+#ifndef __PWM_H 
+#define __PWM_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#define PWM_CYCLE		1200
+#define PWM_OFFSET		200
+
+#define MR0_INT			1 << 0
+#define MR1_INT			1 << 1
+#define MR2_INT			1 << 2
+#define MR3_INT			1 << 3
+#define MR4_INT			1 << 8
+#define MR5_INT			1 << 9
+#define MR6_INT			1 << 10
+
+#define TCR_CNT_EN		0x00000001
+#define TCR_RESET		0x00000002
+#define TCR_PWM_EN		0x00000008
+
+#define PWMMR0I			1 << 0
+#define PWMMR0R			1 << 1
+#define PWMMR0S			1 << 2
+#define PWMMR1I			1 << 3
+#define PWMMR1R			1 << 4
+#define PWMMR1S			1 << 5
+#define PWMMR2I			1 << 6
+#define PWMMR2R			1 << 7
+#define PWMMR2S			1 << 8
+#define PWMMR3I			1 << 9
+#define PWMMR3R			1 << 10
+#define PWMMR3S			1 << 11
+#define PWMMR4I			1 << 12
+#define PWMMR4R			1 << 13
+#define PWMMR4S			1 << 14
+#define PWMMR5I			1 << 15
+#define PWMMR5R			1 << 16
+#define PWMMR5S			1 << 17
+#define PWMMR6I			1 << 18
+#define PWMMR6R			1 << 19
+#define PWMMR6S			1 << 20
+
+#define PWMSEL2			1 << 2
+#define PWMSEL3			1 << 3
+#define PWMSEL4			1 << 4
+#define PWMSEL5			1 << 5
+#define PWMSEL6			1 << 6
+
+#define PWMENA1			1 << 9
+#define PWMENA2			1 << 10
+#define PWMENA3			1 << 11
+#define PWMENA4			1 << 12
+#define PWMENA5			1 << 13
+#define PWMENA6			1 << 14
+
+#define PWMDISA1			0 << 9
+#define PWMDISA2			0 << 10
+#define PWMDISA3			0 << 11
+#define PWMDISA4			0 << 12
+#define PWMDISA5			0 << 13
+#define PWMDISA6			0 << 14
+
+#define LER0_EN			1 << 0
+#define LER1_EN			1 << 1
+#define LER2_EN			1 << 2
+#define LER3_EN			1 << 3
+#define LER4_EN			1 << 4
+#define LER5_EN			1 << 5
+#define LER6_EN			1 << 6
+
+
+#define PWM_INT 				0x40018000	//PWM Interrupt Register
+#define PWM_TIMER_CONTROL 		0x40018004	//PWM Timer Control Register
+#define PWM_COUNT_CONTROL 		0x40018070 	//PWM Count Control Register	
+#define PWM_MATCH_CONTROL 		0x40018014	//PWM Match Register
+#define PWM_CAPTURE_CONTROL 	0x40018028 //PWM Capture Control Register
+#define PWM_Control Register 	0x4001 804C //PWM Control Register
+
+#define PWM_USED	 		1
+#define PWM_NOT_USED 		0
+#define PCONP_REGISTER 		0x400FC0C4
+#define PCLKSEL0_REGISTER 	0x400FC1A8
+#define PWM_COUNT			6			//Anzahl der möglichen PWM
+
+#define PWM1_CLK_ENABLE		(1<<1)
+#define PWM1_CLK_CCLK		(1<<11)	//PCLK_peripheral = CCLK
+#define PCLKSELR0			0x400FC1A8 	//Peripheral Clock Selection register 0
+
+enum pwmCount{PWM1, PWM2, PWM3, PWM4, PWM5, PWM6}; //Auswahl für PWMs
+
+extern void PWM_open(uint8_t pwmNumber);
+extern void PWM_init(uint32_t cycle);
+extern void PWM_set(uint8_t pwm_handle, uint32_t cycle);
+extern void PWM_start(uint8_t pwm_handle);
+extern void PWM_stop(uint8_t pwm_handle);
+
+
+#endif /* end __PWM_H */
